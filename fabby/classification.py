754c30e99fd24db8d29d71837bc4d4d2499a1630
inhueman <fabian.kinyua@gmail.com>
2020-01-28 14:41:38 +0300

    classification trial1

---
+++ fabby/classification.py
@@ -0,0 +1,25 @@
+from sklearn import datasets 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.style.use('ggplot')
+iris = datasets.load_iris()
+type(iris)
+
+print(iris.keys())
+
+
+type(iris.data), type(iris.target)
+
+iris.data.shape
+
+iris.target_names
+
+x = iris.data
+y = iris.target
+
+df = pd.DataFrame(x, column=iris.feature_names)
+print(df.head())
+
+_ = pd.plotting.scatter_matrix(df, c = y, figsize =[8, s = 150, marker = 'D')
